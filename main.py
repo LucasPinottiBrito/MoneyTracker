ee09d037083d52aae3064df98a724e73628e0979
LucasPinottiBrito <lucaspinottibrito@gmail.com>
2023-08-07 23:49:24 -0300

    Favicon, flaskwebgui and images config solved

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import flaskwebgui
+# import sqlite3
+# import time
+# from datetime import datetime
 
 app = Flask(__name__)
 
+gui = flaskwebgui.FlaskUI(app=app, fullscreen=False, server="flask")
 @app.route('/')
-def Home:
-    return '<></>'
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+        gui.run()
